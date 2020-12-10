c391f555ec70d0c7a834ba7b2ead8a15f5f64ead
cvngur <cangur2398@gmail.com>
2020-12-10 22:12:08 +0300

    fixed db file

--- src/db.js
+++ src/db.js
@@ -1,6 +1,14 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
+// Get a Firestore instance
+var firebaseConfig = {
+    apiKey: process.env.FIREBASE_SERVICE_ACCOUNT_QR_MENU_MOBILE,
+    authDomain: "qr-menu-mobile.firebaseapp.com",
+    databaseURL: "https://qr-menu-mobile.firebaseio.com",
+    projectId: "qr-menu-mobile",
+    storageBucket: "qr-menu-mobile.appspot.com",
+};
 export const db = firebase
-    .initializeApp(process.env.FIREBASE_SERVICE_ACCOUNT_QR_MENU_MOBILE)
+    .initializeApp(firebaseConfig)
     .firestore()
