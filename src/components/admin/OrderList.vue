1c29ff0a3994e50d0e1e32b704ca296a2cc6328c
cvngur <cangur2398@gmail.com>
2021-02-03 02:06:32 +0300

    edited orders

--- src/components/admin/OrderList.vue
+++ src/components/admin/OrderList.vue
@@ -1,9 +1,10 @@
 <template>
-  <tr class="flex w-full my-4 flex justify-between">
-    <td class="px-8 w-1/3"> 1 </td>
-    <td class="px-8 w-1/3"> {{ item.tableNumber }}</td>
-    <td class="px-8 w-1/3"> {{ item.orderType }}</td>
-    <td class="px-8 w-1/3"> {{ timeSince(item.time)}}</td>
+  <tr class="text-center">
+    <td class="w-36"> 1</td>
+    <td class="w-36"> {{ item.tableNumber }}</td>
+    <td class="w-36"> {{ item.orderType }}</td>
+    <td class="w-36"> {{ timeSince(item.time) }}</td>
+    <td class="w-36"> Beklemede</td>
   </tr>
 </template>
 
@@ -13,34 +14,34 @@ export default {
   props: {
     item: Object,
   },
-  methods:{
+  methods: {
     timeSince(date) {
 
-  let seconds = Math.floor((new Date() - date) / 1000);
+      let seconds = Math.floor((new Date() - date) / 1000);
 
-  let interval = seconds / 31536000;
+      let interval = seconds / 31536000;
 
-  if (interval > 1) {
-    return Math.floor(interval) + " Sene Önce";
-  }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return Math.floor(interval) + " Ay Önce";
-  }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return Math.floor(interval) + " Gün";
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return Math.floor(interval) + " Saat Önce";
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + " Dakika Önce";
-  }
-  return Math.floor(seconds) + " Saniye Önce";
-}
+      if (interval > 1) {
+        return Math.floor(interval) + " Sene Önce";
+      }
+      interval = seconds / 2592000;
+      if (interval > 1) {
+        return Math.floor(interval) + " Ay Önce";
+      }
+      interval = seconds / 86400;
+      if (interval > 1) {
+        return Math.floor(interval) + " Gün";
+      }
+      interval = seconds / 3600;
+      if (interval > 1) {
+        return Math.floor(interval) + " Saat Önce";
+      }
+      interval = seconds / 60;
+      if (interval > 1) {
+        return Math.floor(interval) + " Dakika Önce";
+      }
+      return Math.floor(seconds) + " Saniye Önce";
+    }
   }
 }
 </script>
--- src/views/admin/Orders.vue
+++ src/views/admin/Orders.vue
@@ -1,23 +1,38 @@
 <template>
-  <div>
+  <div class="flex">
     <Sidebar/>
-    <h2 class="text-right text-2xl my-4 w-96 text-white">Siparişler</h2>
-    <table class="table-fixed mx-72 w-1/3">
-      <thead class="text-white">
-      <tr class="flex mb-4 flex justify-between">
-        <th class="px-8 w-1/3">Sipariş No</th>
-        <th class="px-8 w-1/3">Masa Numarası</th>
-        <th class="px-8 w-1/3">Sipariş Tipi</th>
-        <th class="px-8 w-1/3">Süre</th>
-      </tr>
-      </thead>
-      <hr>
-      <tbody class="text-white w-64">
-      <div v-for="item in items.reverse()" v-bind:key="item.id">
-        <OrderList :item="item"/>
+    <div class="flex-col">
+      <h2 class="text-2xl my-4 text-white -ml-72">Siparişler</h2>
+      <table class="table-fixed ml-72 table-size">
+        <thead>
+        <tr class="flex mb-4 flex justify-between text-black p-4 bg-white rounded-md">
+          <th class="w-36">Sipariş No</th>
+          <th class="w-36">Masa Numarası</th>
+          <th class="w-36">Sipariş Tipi</th>
+          <th class="w-36">Süre</th>
+          <th class="w-36">Sipariş Durumu</th>
+        </tr>
+        </thead>
+        <tbody class="text-white">
+        <div v-for="item in items.reverse()" v-bind:key="item.id">
+          <button class="w-full justify-between my-2 p-4 text-black bg-white rounded-md" v-on:click="selectOrder(item)">
+            <OrderList :item="item"/>
+          </button>
+        </div>
+        </tbody>
+      </table>
+    </div>
+    <div class="flex-col ml-8 my-4">
+      <h2 class="text-2xl text-white">Sipariş Detayı</h2>
+      <div>
+        <div class="text-black p-4 my-4 w-96 bg-white rounded-md">Sipariş No</div>
+        <div class="text-black p-4 my-4 w-96 h-auto bg-white rounded-md">
+          <div v-for="item in detail" v-bind:key="item.id">
+            <OrderDetail :item="item"/>
+          </div>
+        </div>
       </div>
-      </tbody>
-    </table>
+    </div>
   </div>
 </template>
 
@@ -25,24 +40,34 @@
 import Sidebar from '../../components/admin/Sidebar.vue';
 import {db} from "../../db";
 import OrderList from "../../components/admin/OrderList";
+import OrderDetail from "../../components/admin/OrderDetail";
 
 export default {
   name: "Orders",
   components: {
     OrderList,
     Sidebar,
+    OrderDetail,
   },
   data() {
     return {
       items: [],
+      detail: [],
     };
   },
   firestore: {
     items: db.collection("Orders").orderBy('time'),
   },
+  methods: {
+    selectOrder(order) {
+      this.detail = order;
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.table-size {
+  width: 45rem;
+}
 </style>
